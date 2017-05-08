25a3269794e506e9dab1a82ce08e0063310a163e
Josh Strater <jstrater@gmail.com>
2017-05-08 14:07:26 -0700

    Get rid of facts-as-methods.
    Mixing facts with methods introduced the potential for naming conflicts
    (among other problems.)

--- lib/factree/fact_source.rb
+++ lib/factree/fact_source.rb
@@ -1,57 +1,98 @@
 module Factree
   # Mixin to help define fact source classes.
   module FactSource
+    UndefinedFactError = Class.new(StandardError)
+    UnknownFactError = Class.new(StandardError)
+
     def self.included(base)
       base.extend(ClassMethods)
     end
 
     module ClassMethods
-      # Define a fact to be included in the hash by calling a method of the same name.
-      #
-      # If a block is included, it will be used to define a new method for the fact.
+      # Defines a named fact along with a method-like block used to compute the fact's value.
       #
       # @param [Symbol] fact_name The new fact's name
       def def_fact(fact_name, &block)
-        add_fact_names(fact_name)
-        define_method(fact_name, &block) if block
+        all_fact_procs = fact_procs.merge(fact_name => block).freeze
+        class_variable_set(:@@_fact_procs, all_fact_procs)
+        fact_name
       end
 
-      # Adds to the list of facts to be gathered.
-      #
-      # @param [Array<Symbol>] new_fact_names
-      def add_fact_names(*new_fact_names)
-        all_fact_names = fact_names + new_fact_names
-        all_fact_names.uniq!
-        all_fact_names.freeze
-        class_variable_set(:@@_fact_names, all_fact_names)
-      end
-
-      # Names for all of the facts that will be returned by {FactSource#facts}.
+      # @return [Array<Symbol>] Names for all of the facts that will be returned by {FactSource#facts}.
       def fact_names
-        class_variable_defined?(:@@_fact_names) ?
-          class_variable_get(:@@_fact_names) :
-          []
+        fact_procs.keys.freeze
+      end
+
+      # @api private
+      def fact_procs
+        class_variable_defined?(:@@_fact_procs) ?
+          class_variable_get(:@@_fact_procs) :
+          {}.freeze
+      end
+
+      # @return [Boolean] True if the fact has been defined (using {FactSource.def_fact})
+      def defined?(fact_name)
+        fact_procs.include? fact_name
       end
     end
 
-    # A hash mapping all of the fact names to values.
-    def facts
-      self.class.fact_names.map { |fact_name|
-        fact_value = nil
-
-        fact_known = catch(UNKNOWN_FACT) do
-          fact_value = send(fact_name)
-          true
-        end
-
-        fact_known ? [fact_name, fact_value] : nil
-      }.compact.to_h
+    # Checks to see if the value of the fact is known.
+    def known?(fact_name)
+      fetch(fact_name) { return false }
+      true
     end
 
+    # Returns the value of the fact, or nil if the value is unknown
+    def [](fact_name)
+      fetch(fact_name) { nil }
+    end
+
+    # Returns the value of the fact.
+    #
+    # If the value is unknown, then the block will be called with the name of the fact. If no block is supplied, then an UnknownFactError will be raised.
+    def fetch(fact_name, &block)
+      ensure_defined fact_name
+      fact_proc = self.class.fact_procs[fact_name]
+
+      fact_value = nil
+      fact_known = catch(UNKNOWN_FACT) do
+        fact_value = instance_eval(&fact_proc)
+        true
+      end
+      return fact_value if fact_known
+
+      fetch_unknown(fact_name, &block)
+    end
+
+    # @api private
+    def ensure_defined(fact_name)
+      unless self.class.defined? fact_name
+        raise UndefinedFactError, "undefined fact referenced: #{fact_name}"
+      end
+    end
+
+    # @api private
+    def fetch_unknown(fact_name)
+      return yield(fact_name) if block_given?
+
+      raise UnknownFactError, "unknown fact: #{fact_name}"
+    end
+
+    # A hash mapping all of the known fact names to values.
+    def facts
+      self.class.fact_procs.flat_map { |fact_name, _fact_proc|
+        fact_known = true
+        fact_value = fetch(fact_name) { fact_known = false }
+
+        fact_known ? [[fact_name, fact_value]] : []
+      }.to_h
+    end
+
+    # Calling this method in a fact proc will signal that the fact's value is unknown.
     def unknown
       throw UNKNOWN_FACT
     end
 
-    UNKNOWN_FACT = Object.new
+    UNKNOWN_FACT = "Attempted to determine the value of a fact that is unknown"
   end
 end
--- test/factree/fact_source_test.rb
+++ test/factree/fact_source_test.rb
@@ -7,47 +7,81 @@ describe Factree::FactSource do
 
       def_fact(:is_hungry?) { true }
       def_fact(:color) { :brown }
+      def_fact(:texture) { unknown }
     end
   end
   subject { source_class.new }
 
-  describe ".fact" do
-    it "defines methods for facts" do
-      subject.color.must_equal :brown
+  describe ".defined?" do
+    it "is true when a fact has been defined for the class" do
+      assert source_class.defined?(:color)
+    end
+
+    it "is false when a fact has not been defined for the class" do
+      refute source_class.defined?(:height)
     end
   end
 
   describe ".fact_names" do
     it "lists all the defined fact names" do
-      subject.class.fact_names.must_equal [:is_hungry?, :color]
+      assert_equal [:is_hungry?, :color, :texture], subject.class.fact_names
     end
 
     it "is a tasty frozen treat" do
-      subject.class.fact_names.must_be :frozen?
+      assert subject.class.fact_names.frozen?
+    end
+  end
+
+  describe "#known?" do
+    it "is true when the value of a fact is known" do
+      assert subject.known?(:color)
+    end
+
+    it "is false when the value of a fact is unknown" do
+      refute subject.known?(:texture)
+    end
+  end
+
+  describe "#[]" do
+    it "returns a known fact's value" do
+      assert_equal :brown, subject[:color]
+    end
+
+    it "returns nil for an unknown fact" do
+      assert_nil subject[:texture]
+    end
+  end
+
+  describe "#fetch" do
+    it "returns a known fact's value" do
+      assert_equal :brown, subject.fetch(:color)
+    end
+
+    it "raises an error if a fact is unknown and no block is supplied" do
+      assert_raises Factree::FactSource::UnknownFactError do
+        subject.fetch(:texture)
+      end
+    end
+
+    it "calls the block with the fact name if the fact is unknown" do
+      called = false
+      subject.fetch(:texture) { called = true }
+      assert called
+    end
+
+    it "does not deal with undefined facts" do
+      assert_raises Factree::FactSource::UndefinedFactError do
+        subject.fetch(:bogus)
+      end
     end
   end
 
   describe "#facts" do
-    it "returns a hash with all of the fact values" do
-      subject.facts.must_equal(
+    it "returns a hash with all of the known fact values" do
+      assert_equal({
         is_hungry?: true,
         color: :brown
-      )
-    end
-
-    describe "when a fact is unknown" do
-      let(:source_class) do
-        Class.new do
-          include Factree::FactSource
-
-          def_fact(:is_hungry?) { unknown; true }
-          def_fact(:color) { :brown }
-        end
-      end
-
-      it "leaves that fact out of the hash" do
-        subject.facts.must_equal(color: :brown)
-      end
+      }, subject.facts)
     end
   end
 end
